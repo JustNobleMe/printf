dd4dae7447eda461b2d7ade20f60e462251eca7c
name <isaacodey01@gmail.com>
2023-03-30 13:09:22 -0700

    Second printf trial

--- main.h
+++ main.h
@@ -1,54 +1,41 @@
 #ifdef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdarg.h>
-#include <stdio.h>
+int print_char(va_list);
+int print_num(va_list);
+int print_int(va_list);
+int print_str(va_list);
+int print_percent(va_list);
+int print_binary(va_list);
+int print_reversed(va_list);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_hex(va_list);
+int print_HEX(va_list);
+int print_address(va_list);
 
-int _printf(const char *format, ...)
+typedef struct s_printf
 {
-	va_list args;
-	va_start(args, format)
+	char *spe_c;
+	int (*func)(va_list);
+} s_printf;
 
-		int count = 0;
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-
-			switch (*format)
-			{
-				case 'c':
-					putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					count += printf("%s", va_arg(args, char *));
-					break;
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				case 'd':
-				case 'i':
-					count += printf("%d", va_arg(args, int));
-					break;
-				default:
-					break;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
-
-	va_end(args);
-
-	return count;
-}
+s_printf func_arr[] = {
+	{"c", print_char},
+	{"s", print_str},
+	{"%", print_percentage},
+	{"d", print_int},
+	{"i", print_int},
+	{"b", print_binary},
+	{"r", print_reversed},
+	{"R", rot13},
+	{"u", unsigned_integer},
+	{"o", print_octal},
+	{"x", print_hex},
+	{"X", print_HEX},
+	{"p", print_address},
+	{NULL, NULL}
+},
+int select_f(const char *format, s_printf func_arr[], va_list arg_list);
 
 #endif
